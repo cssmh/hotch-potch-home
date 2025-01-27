cd1bed1f2308f46b85831e521d1c890c09b02518
Momen <touristmomen@gmail.com>
2025-01-27 17:46:51 +0600

    Mixland button left issue removing custom button

--- src/components/MixlandBtn.tsx
+++ src/components/MixlandBtn.tsx
@@ -1,6 +1,5 @@
 import { misLandBtnData } from "@/data";
 import Link from "next/link";
-import CustomButton from "./ui/CustomButton";
 
 export default function MislandBtn() {
   return (
@@ -8,15 +7,15 @@ export default function MislandBtn() {
       {misLandBtnData.map((btn, idx) => (
         <div key={idx} className="w-[290px] md:max-w-[302px]">
           <Link href={btn.link}>
-            <CustomButton
-              textLabel={btn.label}
-              btnBgColor={idx === 0 ? "primary-gradient" : "bg-transparent"}
-              borderColor={
+            <div
+              className={`rounded-lg cursor-pointer relative flex justify-start items-center px-6 py-3 ${
                 idx === 0
-                  ? "border border-gradient"
-                  : "border border-gray-400 hover:border-accent transition-all duration-700 ease-in"
-              }
-            />
+                  ? "bg-gradient-to-r from-gradient to-gradient-foreground"
+                  : "bg-transparent border border-[#282a37] hover:border-accent transition-all duration-700 ease-in"
+              }`}
+            >
+              <span className="text-white">{btn.label}</span>
+            </div>
           </Link>
         </div>
       ))}
