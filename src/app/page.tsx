3b1b14a01f96b9262ddcb589cb269d0a5009c058
Momen <touristmomen@gmail.com>
2025-01-27 13:07:33 +0600

    Content in home added with type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/Banner";
 import Company from "@/components/Company";
+import Content from "@/components/Content";
 
 export default function Home() {
   return (
@@ -11,6 +12,26 @@ export default function Home() {
         <Banner />
       </div>
       <Company />
+      <div className="section-padding flex flex-col gap-8 xl:gap-0 pb-8 md:pb-5 lg:pb-0">
+        <Content
+          key={1}
+          className="flex flex-col md:flex-row items-center justify-evenly"
+          imgSrc="/assets/images/trial/trial-img-1.webp"
+          heading="Create content efficiently and quickly with great AI writing tools"
+          desc="150k+ users. No Credit Card Required. Pro designs and writing at no cost. Start for free. Ai Writer Tool | Generate text for ecom, social media, website, sales, blogs etc."
+          imgStyle="-right-7 md:-right-0 md:-left-8 lg:-left-12 xl:-left-16 2xl:-left-20 md:h-[350px] lg:h-[450px] xl:h-[530px] 2xl:h-[650px] md:object-contain xl:object-none"
+          blobStyle="left-[100px] lg:left-[200px] xl:left-[280px] 2xl:left-[350px] top-0 bg-[#a136f214]"
+        />
+        <Content
+          key={2}
+          className="flex flex-col md:flex-row-reverse items-center justify-evenly"
+          imgSrc="/assets/images/trial/trial-img-2.webp"
+          heading="Create content efficiently and quickly with great AI writing tools"
+          desc="150k+ users. No Credit Card Required. Pro designs and writing at no cost. Start for free. Ai Writer Tool | Generate text for ecom, social media, website, sales, blogs etc."
+          imgStyle="-right-0 md:-right-8 lg:-right-12 xl:-right-16 2xl:-right-20 md:h-[350px] lg:h-[450px] xl:h-[530px] 2xl:h-[650px] md:object-contain xl:object-none"
+          blobStyle="right-[100px] lg:right-[240px] xl:right-[300px] 2xl:right-[400px] -top-5 md:top-0 2xl:top-10 bg-[#366bf215]"
+        />
+      </div>
     </main>
   );
 }
---
+++ src/components/Content.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import Link from "next/link";
+import CustomButton from "./ui/CustomButton";
+
+interface ContentProps {
+  className?: string;
+  imgSrc: string;
+  heading: string;
+  desc: string;
+  imgStyle: string;
+  blobStyle: string;
+}
+
+const Content: React.FC<ContentProps> = ({
+  className,
+  imgSrc,
+  heading,
+  desc,
+  imgStyle,
+  blobStyle,
+}) => {
+  return (
+    <section className="main-container">
+      <div className={`${className} relative 2xl:px-44`}>
+        {/* blob blur */}
+        <div
+          className={`absolute w-[200px] h-[200px] md:w-[300px] xl:w-[400px] md:h-[300px] xl:h-[400px] rounded-full blur-3xl ${blobStyle}`}
+        />
+        <Image
+          src={imgSrc}
+          alt="Seo content written by ai"
+          width={690}
+          height={650}
+          className={`relative z-10 ${imgStyle}`}
+        />
+        <div className="flex flex-col gap-4 lg:gap-7">
+          <h2 className="sub-heading max-w-[497px] font-semibold xl:leading-tight 2xl:leading-tight">
+            {heading}
+          </h2>
+          <p className="desc-text text-secondary/70 max-w-[487px]">{desc}</p>
+          <div className="max-w-[180px] lg:max-w-[220px] mt-3">
+            <Link href={"/"}>
+              <CustomButton
+                key={1}
+                textLabel="Get started free"
+                btnBgColor="primary-gradient"
+                borderColor="border border-gradient"
+                hoverColor1="bg-primary"
+                hoverColor2="bg-[#FFC947]"
+                hoverColor3="accent-gradient"
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Content;
